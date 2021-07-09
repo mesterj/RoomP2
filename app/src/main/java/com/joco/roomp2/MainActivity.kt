20243b1e15da4d43c2ac55a022de05af6fe0c8aa
mester <mester@kite.hu>
2021-07-09 13:39:18 +0200

    room2 end of video 2

--- app/src/main/java/com/joco/roomp2/MainActivity.kt
+++ app/src/main/java/com/joco/roomp2/MainActivity.kt
@@ -2,10 +2,20 @@ package com.joco.roomp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        setupActionBarWithNavController(findNavController(R.id.fragmentContainerView2))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView2)
+        return navController.navigateUp() ||  super.onSupportNavigateUp()
     }
 }
